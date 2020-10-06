45b55c2451d7a6c2f1e44d2dc572fc14eed2d1df
Lucas Feijo <feijolucas1997@gmail.com>
2020-10-05 21:03:38 -0300

    feat: implement Gif dao based on Giphy API response

--- internal/domain/gif/gif_dao.go
+++ internal/domain/gif/gif_dao.go
@@ -1,1 +1,17 @@
 package gif
+
+// Gif model definition from Giphy
+type Gif struct {
+	Title  string `json:"title"`
+	Images Images `json:"images"`
+}
+
+// Images type definition from Giphy
+type Images struct {
+	Original Original `json:"original"`
+}
+
+// Original image type definition from Giphy
+type Original struct {
+	URL string `json:"url"`
+}
