46be2850e049a1a9520bf658a0543bc4cb2d9062
Lucas Feijo <feijolucas1997@gmail.com>
2020-10-07 00:44:18 -0300

    feat: implemented ingredient-based recipe search functionality, with tests (93.9%)

--- pkg/services/recipeservice/recipe_service.go
+++ pkg/services/recipeservice/recipe_service.go
@@ -1,1 +1,93 @@
 package recipeservice
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/flucas97/delivery-much-challange/internal/domain/recipe"
+	"github.com/flucas97/delivery-much-challange/pkg/services/gifservice"
+	"github.com/flucas97/delivery-much-challange/tools/errortools"
+)
+
+var (
+	// RecipeService interface with other layers
+	RecipeService recipeServiceInterface = &recipeService{}
+
+	// RecipeURI recipepuppy API endpoint
+	RecipeURI = "http://www.recipepuppy.com/api/?i="
+)
+
+type recipeServiceInterface interface {
+	GetAll([]string) ([]recipe.Recipe, *errortools.APIError)
+}
+
+type recipeService struct{}
+
+func (rs *recipeService) GetAll(ingredients []string) ([]recipe.Recipe, *errortools.APIError) {
+	if len(ingredients) > 3 {
+		return nil, errortools.APIErrorInterface.NewBadRequestError("max of 3 ingredients. recipeservice.GetAll")
+	}
+
+	ingredientsConcatenated := rs.concatenateIngredients(ingredients)
+	request, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s%s", RecipeURI, ingredientsConcatenated), nil)
+	if err != nil {
+		return nil, errortools.APIErrorInterface.NewInternalServerError("error mounting request. recipeservice.GetAll")
+	}
+
+	client := &http.Client{}
+
+	resp, err := client.Do(request)
+	if err != nil {
+		return nil, errortools.APIErrorInterface.NewInternalServerError("error doing request. recipeservice.GetAll")
+	}
+	defer resp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errortools.APIErrorInterface.NewInternalServerError("error reading body. recipeservice.GetAll")
+	}
+
+	var sr recipe.SearchResult
+
+	if err := json.Unmarshal(bytes, &sr); err != nil {
+		return nil, errortools.APIErrorInterface.NewInternalServerError("error unmarshalling response from client. recipeservice.GetAll")
+	}
+
+	recipes := sr.IngredientsToSortedSlice()
+
+	for _, recipe := range recipes {
+		rs.getGifToRecipe(&recipe)
+	}
+
+	return recipes, nil
+}
+
+func (rs *recipeService) concatenateIngredients(ingredients []string) string {
+	var (
+		result  string
+		counter int
+	)
+
+	for _, ingredient := range ingredients {
+		counter++
+		if counter == len(ingredients) {
+			result += ingredient
+		} else {
+			result += ingredient + ","
+		}
+	}
+
+	return result
+}
+
+func (rs *recipeService) getGifToRecipe(recipe *recipe.Recipe) *errortools.APIError {
+	gif, err := gifservice.GifService.GetRandomByTag(recipe.Title)
+	if err != nil {
+		return err
+	}
+
+	recipe.Gif = gif.Images.Original.URL
+	return nil
+}
---
+++ pkg/services/recipeservice/recipe_service_test.go
@@ -0,0 +1,118 @@
+package recipeservice
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/flucas97/delivery-much-challange/internal/domain/recipe"
+	"github.com/flucas97/delivery-much-challange/pkg/services/gifservice"
+	"github.com/jarcoal/httpmock"
+	"github.com/stretchr/testify/assert"
+)
+
+var rs recipeService
+
+func TestGetRandomByTagRestClientNoError(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+	httpmock.RegisterResponder("GET", RecipeURI+"eggs,garlic,onions", httpmock.NewStringResponder(http.StatusOK,
+		`{
+			"results":[
+				{
+					   "title":"Vegetable-Pasta Oven Omelet",
+					   "href":"http:\/\/find.myrecipes.com\/recipes\/recipefinder.dyn?action=displayRecipe&recipe_id=520763",
+					   "ingredients":"tomato, onions, red pepper, garlic"
+				}
+			]
+		}`))
+
+	var ingredients = []string{"eggs", "garlic", "onions"}
+	result, err := rs.GetAll(ingredients)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, result)
+	assert.Equal(t, "Vegetable-Pasta Oven Omelet", result[0].Title)
+	assert.Equal(t, []string{"garlic", "onions", "red pepper", "tomato"}, result[0].Ingredients)
+}
+
+func TestGetRandomByTagRestClientWithFourIngredients(t *testing.T) {
+	var ingredients = []string{"eggs", "garlic", "onions", "açai"}
+	result, err := rs.GetAll(ingredients)
+
+	assert.Nil(t, result)
+	assert.NotNil(t, err)
+	assert.Equal(t, "max of 3 ingredients. recipeservice.GetAll", err.Message)
+}
+
+func TestGetRandomByTagRestClientErrorDoingRequest(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	RecipeURI = "fake.com.br"
+	httpmock.RegisterResponder("GET", RecipeURI+"[]", httpmock.NewStringResponder(http.StatusInternalServerError, ""))
+
+	var ingredients = []string{""}
+	result, err := rs.GetAll(ingredients)
+
+	assert.Nil(t, result)
+	assert.NotNil(t, err)
+	assert.Equal(t, "error doing request. recipeservice.GetAll", err.Message)
+}
+
+func TestGetRandomByTagRestClientUnmarshallingError(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+	httpmock.RegisterResponder("GET", RecipeURI+"eggs,garlic,onions", httpmock.NewStringResponder(http.StatusOK,
+		`{
+			"results":[
+				{
+					   "title":"Vegetable-Pasta Oven Omelet",
+					   "href":"http:\/\/find.myrecipes.com\/recipes\/recipefinder.dyn?action=displayRecipe&recipe_id=520763",
+					   "ingredients":"tomato, onions, red pepper, garlic",
+				}
+			]
+		}`))
+
+	var ingredients = []string{"eggs", "garlic", "onions"}
+	result, err := rs.GetAll(ingredients)
+
+	assert.Nil(t, result)
+	assert.NotNil(t, err)
+	assert.Equal(t, "error unmarshalling response from client. recipeservice.GetAll", err.Message)
+}
+
+func TestConcatenateIngredients(t *testing.T) {
+	var (
+		ingredients = []string{"aveia", "mel", "abobora"}
+	)
+
+	result := rs.concatenateIngredients(ingredients)
+	assert.Equal(t, "aveia,mel,abobora", result)
+}
+
+func TestGetGifToRecipe(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+	httpmock.RegisterResponder("GET", fmt.Sprintf(gifservice.GiphyURI, "dev", os.Getenv("GIPHY_API_KEY")), httpmock.NewStringResponder(http.StatusOK,
+		`{
+			"title": "dev",
+			"images": {
+				"original": {
+					"url": "https://delivery-much-challange-tag.gif"
+				}	
+			}
+		}`,
+	))
+
+	recipe := recipe.Recipe{
+		Title: "dev",
+	}
+
+	err := rs.getGifToRecipe(&recipe)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, recipe.Gif)
+	assert.Equal(t, "https://delivery-much-challange-tag.gif", recipe.Gif)
+}
