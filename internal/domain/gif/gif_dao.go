86b97d2ecda1ac5e79e8b090992501ed041f6640
Lucas Feijo <feijolucas1997@gmail.com>
2020-10-07 23:45:36 -0300

    refactor: simplifying Gif model

--- internal/domain/gif/gif_dao.go
+++ internal/domain/gif/gif_dao.go
@@ -1,17 +1,6 @@
 package gif
 
-// Gif model definition from Giphy
+// Gif model definition
 type Gif struct {
-	Title  string `json:"title"`
-	Images Images `json:"images"`
-}
-
-// Images type definition from Giphy
-type Images struct {
-	Original Original `json:"original"`
-}
-
-// Original image type definition from Giphy
-type Original struct {
 	URL string `json:"url"`
 }
