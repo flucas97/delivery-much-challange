44cd0e3c265c84012c53d488f23a91fad50cc9b8
Lucas Feijo <feijolucas1997@gmail.com>
2020-10-07 23:50:43 -0300

    refactor: changing logic, using external package to request a gif from the Giphy API, with tests

--- pkg/services/gifservice/gif_service.go
+++ pkg/services/gifservice/gif_service.go
@@ -1,14 +1,11 @@
 package gifservice
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
 	"os"
 
 	"github.com/flucas97/delivery-much-challange/internal/domain/gif"
 	"github.com/flucas97/delivery-much-challange/tools/errortools"
+	libgiphy "github.com/sanzaru/go-giphy"
 )
 
 var (
@@ -22,43 +19,22 @@ var (
 )
 
 type gifServiceInterface interface {
-	GetRandomByTag(tag string) (*gif.Gif, *errortools.APIError)
+	GetRandom(tag string) (*gif.Gif, *errortools.APIError)
 }
 
 type gifService struct{}
 
-// GetRandomByTag is responsible for getting a Giphy Gif based on a tag
-func (gs *gifService) GetRandomByTag(tag string) (*gif.Gif, *errortools.APIError) {
-	var (
-		client     = &http.Client{}
-		prepareURL = fmt.Sprintf(GiphyURL, tag, os.Getenv("GIPHY_API_KEY"))
-	)
+func (gs *gifService) GetRandom(tag string) (*gif.Gif, *errortools.APIError) {
+	giphyClient := libgiphy.NewGiphy(os.Getenv("GIPHY_API_KEY"))
 
-	request, err := http.NewRequest(http.MethodGet, prepareURL, nil)
+	random, err := giphyClient.GetRandom(tag)
 	if err != nil {
-		return nil, errortools.APIErrorInterface.NewInternalServerError("error mounting request. gifservice.GetRandomByTag")
+		return nil, errortools.APIErrorInterface.NewInternalServerError("error getting Giphy. gifservice.GetRandom")
 	}
 
-	resp, err := client.Do(request)
-	if err != nil {
-		return nil, errortools.APIErrorInterface.NewInternalServerError("error doing request. gifservice.GetRandomByTag")
-	}
-	defer resp.Body.Close()
-
-	bytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, errortools.APIErrorInterface.NewInternalServerError("error reading body. gifservice.GetRandomByTag")
+	var result = &gif.Gif{
+		URL: random.Data.Image_original_url,
 	}
 
-	var result gif.Gif
-
-	if err := json.Unmarshal(bytes, &result); err != nil {
-		return nil, errortools.APIErrorInterface.NewInternalServerError("error unmarshalling response from client. gifservice.GetRandomByTag")
-	}
-
-	if result.Images.Original.URL == "" {
-		return nil, errortools.APIErrorInterface.NewInternalServerError("no response from client. gifservice.GetRandomByTag")
-	}
-
-	return &result, nil
+	return result, nil
 }
--- pkg/services/gifservice/gif_service_test.go
+++ pkg/services/gifservice/gif_service_test.go
@@ -6,95 +6,22 @@ import (
 	"os"
 	"testing"
 
-	"github.com/flucas97/delivery-much-challange/internal/domain/gif"
 	"github.com/jarcoal/httpmock"
 	"github.com/stretchr/testify/assert"
 )
 
-var gs gifService
+var (
+	gs gifService
+)
 
-func TestGetRandomByTag(t *testing.T) {
-	t.Run("Giphy without original URL", func(t *testing.T) {
+func TestGetRandom(t *testing.T) {
+	t.Run("Error receiving a Giphy", func(t *testing.T) {
 		httpmock.Activate()
 		defer httpmock.DeactivateAndReset()
+		httpmock.RegisterResponder("GET", fmt.Sprintf("http://api.giphy.com/v1/gifs/random?api_key=%s", os.Getenv("GIPHY_API_KEY")), httpmock.NewStringResponder(http.StatusInternalServerError, `{error}`))
 
-		httpmock.RegisterResponder("GET", fmt.Sprintf(GiphyURL, "mychallange", os.Getenv("GIPHY_API_KEY")), httpmock.NewStringResponder(http.StatusOK,
-			`{
-				"title": "",
-				"images": {
-					"original": {
-						"url": ""
-					}	
-				}
-			}`,
-		))
-
-		result, err := gs.GetRandomByTag("mychallange")
-		assert.NotNil(t, err)
-		assert.Nil(t, result)
-		assert.Equal(t, "no response from client. gifservice.GetRandomByTag", err.Message)
-	})
-
-	t.Run("Success receiving a Giphy", func(t *testing.T) {
-		httpmock.Activate()
-		defer httpmock.DeactivateAndReset()
-		httpmock.RegisterResponder("GET", fmt.Sprintf(GiphyURL, "tag", os.Getenv("GIPHY_API_KEY")), httpmock.NewStringResponder(http.StatusOK,
-			`{
-				"title": "tag",
-				"images": {
-					"original": {
-						"url": "https://delivery-much-challange-tag.gif"
-					}	
-				}
-			}`,
-		))
-
-		result, err := GifService.GetRandomByTag("tag")
-
-		expected := &gif.Gif{
-			Title: "tag",
-			Images: gif.Images{
-				Original: gif.Original{
-					URL: "https://delivery-much-challange-tag.gif",
-				},
-			},
-		}
-
-		assert.Nil(t, err)
-		assert.Equal(t, expected, result)
-	})
-	t.Run("Error unmarshalling response from client", func(t *testing.T) {
-		httpmock.Activate()
-		defer httpmock.DeactivateAndReset()
-		httpmock.RegisterResponder("GET", fmt.Sprintf(GiphyURL, "tag", os.Getenv("GIPHY_API_KEY")), httpmock.NewStringResponder(http.StatusOK,
-			`{
-				"title": "tag",
-				"images": {
-					"original": {
-						"url": "https://delivery-much-challange-tag.gif"
-					},
-				}
-			}`,
-		))
-
-		result, err := gs.GetRandomByTag("tag")
-
-		assert.NotNil(t, err)
-		assert.Empty(t, result, "unmarshal should got error")
-	})
-
-	t.Run("Error mounting request", func(t *testing.T) {
-		httpmock.Activate()
-		defer httpmock.DeactivateAndReset()
-
-		GiphyURL = "fake.com.br"
-
-		httpmock.RegisterResponder("GET", fmt.Sprintf(GiphyURL, "tag", os.Getenv("GIPHY_API_KEY")), httpmock.NewStringResponder(http.StatusInternalServerError, ""))
-
-		result, err := gs.GetRandomByTag("tag")
-
-		assert.Equal(t, "error mounting request. gifservice.GetRandomByTag", err.Message)
-		assert.NotNil(t, err)
-		assert.Empty(t, result, "mounting request should got error")
+		gif, err := gs.GetRandom("")
+		assert.Equal(t, "error getting Giphy. gifservice.GetRandom", err.Message)
+		assert.Nil(t, gif)
 	})
 }
